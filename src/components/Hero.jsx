74dd60d4a5ed366b7cd9a6da7c80478f865b6980
Baashe <baashe@Baashes-MacBook-Air.local>
2025-10-23 16:44:03 +0300

    i changed the content of componenets

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react';
+import { heroVideo, smallHeroVideo } from '../utils';
+import { useState } from 'react';
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
 
-export default Hero
+    useGSAP(() => {
+        gsap.to('#hero', { opacity: 1, delay: 1.5 })
+    }, [])
+
+  return (
+    <section className="w-full nav-height bg-black">
+      <div className="h-5/ w-full flex-center flex-col">
+        <p id="hero" className="hero-title">iPhone 15 </p>
+        <div className='md:w-10/12 w-9/12'>
+            <video src={videoSrc}>
+
+            </video>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
